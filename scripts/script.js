db7d0d4818f4ea5d06378f92cec48074c18c0c76
Guilherme Saud <gsaud1008@gmail.com>
2023-11-29 22:45:59 -0300

    feat: Responsive carousel
    Added responsive carousel in four different sizes based on navigator width and height properties
    
    Created two functions which are loading the images based on navigator width and height

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
-//count representa qual radio button está selecionado (qual imagem está sendo mostrada)
+//? count representa qual radio button está selecionado (qual imagem está sendo mostrada)
 let count = 1;
 document.getElementById("radio1").checked = true;
 
-//Tempo de transição entre as imagens
+//! Tempo de transição entre as imagens
 setInterval(() => {
   nextImage();
 }, 4000);
@@ -16,8 +16,8 @@ function nextImage() {
   document.getElementById("radio"+count).checked = true;
 }
 
+//! Scroll suave
 document.addEventListener("DOMContentLoaded", function () {
-  //Scroll suave
   const botoesScroll = document.querySelectorAll(".header a[href^='#']"); 
   const header = document.getElementById("header");
   const alturaHeader = header.offsetHeight;
@@ -55,3 +55,87 @@ document.addEventListener("DOMContentLoaded", function () {
     link.addEventListener("click", scrollToSection);
   });
 });
+
+//! Mudando imagem do carrossel de acordo com o tamanho da tela:
+function obterCaminhoImagem(resolucao, numeroImagem) {
+  var paths = {
+      '320': 'img/carrossel/320/',
+      '400': 'img/carrossel/400/',
+      '768': 'img/carrossel/768/',
+      '1280': 'img/carrossel/1280/',
+      'Desktop': 'img/carrossel/Desktop/'
+  };
+
+  if (!paths[resolucao]) {
+      console.error('Resolução inválida: ' + resolucao);
+      return '';
+  }
+
+  var caminhoImagem = `${paths[resolucao]}${numeroImagem}_${resolucao}p.png`;
+
+  return caminhoImagem;
+}
+
+let imagensCarrossel1 = document.getElementById('imagemCarrossel1');
+let imagensCarrossel2 = document.getElementById('imagemCarrossel2');
+let imagensCarrossel3 = document.getElementById('imagemCarrossel3');
+let imagensCarrossel4 = document.getElementById('imagemCarrossel4');
+
+//Para definir a altura da imagem
+var imagensSlides = document.querySelectorAll('.slide img');
+
+function ajustarImagensETamanho() {
+  var tamanho = window.innerWidth;
+
+  if ((tamanho <= 1920 && tamanho > 1280) || tamanho > 1920) {
+    imagensCarrossel1.src = obterCaminhoImagem('Desktop', 1);
+    imagensCarrossel2.src = obterCaminhoImagem('Desktop', 2);
+    imagensCarrossel3.src = obterCaminhoImagem('Desktop', 3);
+    imagensCarrossel4.src = obterCaminhoImagem('Desktop', 4);
+
+    imagensSlides.forEach(function (imagemSlide) {
+      imagemSlide.style.height = '100%';
+    });
+  } else if (tamanho <= 1280 && tamanho > 768) {
+    imagensCarrossel1.src = obterCaminhoImagem('1280', 1);
+    imagensCarrossel2.src = obterCaminhoImagem('1280', 2);
+    imagensCarrossel3.src = obterCaminhoImagem('1280', 3);
+    imagensCarrossel4.src = obterCaminhoImagem('1280', 4);
+
+    imagensSlides.forEach(function (imagemSlide) {
+      imagemSlide.style.height = '100%';
+    });
+  } else if (tamanho <= 768 && tamanho > 400) {
+    imagensCarrossel1.src = obterCaminhoImagem('768', 1);
+    imagensCarrossel2.src = obterCaminhoImagem('768', 2);
+    imagensCarrossel3.src = obterCaminhoImagem('768', 3);
+    imagensCarrossel4.src = obterCaminhoImagem('768', 4);
+
+    imagensSlides.forEach(function (imagemSlide) {
+      imagemSlide.style.height = '40%';
+    });
+  } else if (tamanho <= 400 && tamanho > 320) {
+    imagensCarrossel1.src = obterCaminhoImagem('400', 1);
+    imagensCarrossel2.src = obterCaminhoImagem('400', 2);
+    imagensCarrossel3.src = obterCaminhoImagem('400', 3);
+    imagensCarrossel4.src = obterCaminhoImagem('400', 4);
+
+    imagensSlides.forEach(function (imagemSlide) {
+      imagemSlide.style.height = '20%';
+    });
+  } else {
+    imagensCarrossel1.src = obterCaminhoImagem('320', 1);
+    imagensCarrossel2.src = obterCaminhoImagem('320', 2);
+    imagensCarrossel3.src = obterCaminhoImagem('320', 3);
+    imagensCarrossel4.src = obterCaminhoImagem('320', 4);
+
+    imagensSlides.forEach(function (imagemSlide) {
+      imagemSlide.style.height = '20%';
+    });
+  }
+}
+
+ajustarImagensETamanho();
+
+// Adicionar o evento de redimensionamento
+window.addEventListener('resize', ajustarImagensETamanho);
